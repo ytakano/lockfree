10498ba44fabb8f0a5d3ba75080889eabc37f154
Yuuki Takano <ytakanoster@gmail.com>
2020-12-26 18:01:04 +0900

    separate file

--- src/main.rs
+++ src/main.rs
@@ -1,82 +1,12 @@
-use std::ptr::null_mut;
-use std::sync::atomic::{AtomicPtr, Ordering};
 use std::sync::Arc;
+
+mod stack;
 
 const NUM_LOOP: usize = 1000000;
 const NUM_THREADS: usize = 4;
 
-struct Node<T> {
-    next: AtomicPtr<Node<T>>,
-    data: T,
-}
-
-struct Stack<T> {
-    head: AtomicPtr<Node<T>>,
-}
-
-impl<T> Stack<T> {
-    fn new() -> Stack<T> {
-        Stack {
-            head: AtomicPtr::new(null_mut()),
-        }
-    }
-
-    fn push(&self, v: T) {
-        let node = Box::new(Node {
-            next: AtomicPtr::new(null_mut()),
-            data: v,
-        });
-        let ptr = Box::into_raw(node);
-
-        unsafe {
-            loop {
-                let head = self.head.load(Ordering::Relaxed);
-                (*ptr).next.store(head, Ordering::Relaxed);
-                if let Ok(_) =
-                    self.head
-                        .compare_exchange_weak(head, ptr, Ordering::Release, Ordering::Relaxed)
-                {
-                    break;
-                }
-            }
-        }
-    }
-
-    fn pop(&self) -> Option<T> {
-        unsafe {
-            loop {
-                let head = self.head.load(Ordering::Relaxed);
-                if head == null_mut() {
-                    return None;
-                }
-
-                let next = (*head).next.load(Ordering::Relaxed);
-                if let Ok(_) = self.head.compare_exchange_weak(
-                    head,
-                    next,
-                    Ordering::Acquire,
-                    Ordering::Relaxed,
-                ) {
-                    let h = Box::from_raw(head);
-                    return Some((*h).data);
-                }
-            }
-        }
-    }
-}
-
-impl<T> Drop for Stack<T> {
-    fn drop(&mut self) {
-        let mut node = self.head.load(Ordering::Relaxed);
-        while node != null_mut() {
-            let n = unsafe { Box::from_raw(node) };
-            node = n.next.load(Ordering::Relaxed)
-        }
-    }
-}
-
 fn main() {
-    let stack = Arc::new(Stack::<usize>::new());
+    let stack = Arc::new(stack::Stack::<usize>::new());
     let mut v = Vec::new();
 
     for i in 0..NUM_THREADS {
