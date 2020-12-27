fab093ffe6d526e6b8dfeee0f82c493271a7480b
Yuuki Takano <ytakanoster@gmail.com>
2020-12-27 15:59:43 +0000

    resolved ABA problem

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
+#![feature(asm)]
+
 use std::sync::Arc;
 
-mod stack;
+mod lockfree;
+// mod stack;
+mod stack_aba;
 
 const NUM_LOOP: usize = 1000000;
 const NUM_THREADS: usize = 4;
 
+use lockfree::LockFree;
+use stack_aba::Stack;
+
 fn main() {
-    let stack = Arc::new(stack::Stack::<usize>::new());
+    let stack = Arc::new(LockFree::new(Stack::<usize>::new()));
     let mut v = Vec::new();
 
     for i in 0..NUM_THREADS {
@@ -15,14 +22,14 @@ fn main() {
             if i & 1 == 0 {
                 for j in 0..NUM_LOOP {
                     let k = i * NUM_LOOP + j;
-                    (*stack0).push(k);
+                    stack0.get().push(k);
                     println!("push: {}", k);
                 }
                 println!("finished push: #{}", i);
             } else {
                 for _ in 0..NUM_LOOP {
                     loop {
-                        if let Some(k) = (*stack0).pop() {
+                        if let Some(k) = stack0.get().pop() {
                             println!("pop: {}", k);
                             break;
                         }
